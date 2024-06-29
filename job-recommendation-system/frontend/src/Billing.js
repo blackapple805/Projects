0a56da41f1d9ad57aa0f7102b35f2b3d432cd11f
Eric Del Angel <73086687+blackapple805@users.noreply.github.com>
2024-06-29 19:52:35 +0000

    updated my biliing methold and also added plans for the program, need to update the colors still

--- job-recommendation-system/frontend/src/Billing.js
+++ job-recommendation-system/frontend/src/Billing.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import './Billing.css';
 
 function Billing() {
   return (
-    <div>
+    <div className="billing-container">
       <h2>Billing</h2>
-      <p>This is the Billing page.</p>
+      <p>Enter your credit card details to upgrade your plan or remove it.</p>
+      <form className="form">
+        <label className="label">
+          <span className="title">Card holder full name</span>
+          <input className="input-field" type="text" placeholder="Enter your full name" />
+        </label>
+        <label className="label">
+          <span className="title">Card Number</span>
+          <input className="input-field" type="number" placeholder="0000 0000 0000 0000" />
+        </label>
+        <div className="split">
+          <label className="label">
+            <span className="title">Expiry Date</span>
+            <input className="input-field" type="text" placeholder="MM/YY" />
+          </label>
+          <label className="label">
+            <span className="title">CVV</span>
+            <input className="input-field" type="number" placeholder="CVV" />
+          </label>
+        </div>
+        <input className="checkout-btn" type="button" value="Checkout" />
+      </form>
     </div>
   );
 }
--- job-recommendation-system/frontend/src/Email.js
+++ job-recommendation-system/frontend/src/Email.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Email.css';
 
 function Email() {
   return (
-    <div>
+    <div className="email-container">
       <h2>Email</h2>
       <p>This is the Email page.</p>
     </div>
