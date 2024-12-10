eb1a2fe20f6e9176eb5f4393ba24f45f288d3bd0
Blackapple805 <Blackapple805@gmail.com>
2024-12-10 13:23:39 -0800

    Added some changes to the site so it looks good - Eric D Angel

--- tinaswebsite/frontend/tailwind.config.js
+++ tinaswebsite/frontend/tailwind.config.js
@@ -1,8 +1,8 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./index.html", 
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
