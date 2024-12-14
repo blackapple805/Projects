3f0849cfda18b3c671817d9176568f33fb736e8b
Blackapple805 <Blackapple805@gmail.com>
2024-12-14 08:59:04 -0800

    Added alot of changes to the front page of the website thats all - Eric Del Angel

--- tinaswebsite/frontend/src/App.jsx
+++ tinaswebsite/frontend/src/App.jsx
@@ -1,34 +1,36 @@
-// frontend/src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'aos/dist/aos.css';
-// frontend/src/App.jsx
 import AOS from 'aos';
 import Home from './pages/Home'; 
 import Products from './pages/Products'; 
 import Register from './pages/Register'; 
 import Header from './components/Header';
-import Footer from './components/Footer';
+// Footer import removed
+// import Footer from './components/Footer';
 
 AOS.init({ duration: 800 });
 
 function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen bg-gray-900 text-white relative">
+      {/* Removed dark backgrounds, using white instead */}
+      <div className="flex flex-col min-h-screen bg-white text-black relative">
         <Header />
-        {/* Add top padding so content doesn't hide behind the fixed header */}
-        <main className="flex-grow p-4 pt-20 pb-20">
+        {/* If you want the white background behind everything, no dark backgrounds here */}
+        <main className="flex-grow p-4 pt-20 pb-20 bg-white"> 
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
             <Route path="/register" element={<Register />} />
           </Routes>
         </main>
-        <Footer />
+        {/* Footer removed */}
+        {/* <Footer /> */}
       </div>
     </Router>
   );
 }
 
 export default App;
+
 
